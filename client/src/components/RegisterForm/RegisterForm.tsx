4be972803b9e6e0d58ed25bcfa08bb4a3067d5ae
Igor Storchevoy <hellsgor@gmail.com>
2024-07-16 23:50:44 +0400

    Update(RegistrationForm): basic form submit

--- client/src/components/RegisterForm/RegisterForm.tsx
+++ client/src/components/RegisterForm/RegisterForm.tsx
@@ -1,20 +1,50 @@
-import { FormField } from "../FormField";
-import { Button } from "../Button";
-import "./RegisterForm.css";
+import { FormField } from '../FormField';
+import { Button } from '../Button';
+import './RegisterForm.css';
+import { FC, FormEventHandler, useState } from 'react';
 
-export const RegisterForm = () => {
+export const RegisterForm: FC = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className="register-form">
+    <form className="register-form" onSubmit={handleSubmit}>
       <FormField label="Имя">
-        <input />
+        <input
+          type="text"
+          name="username"
+          onChange={(event) => setUsername(event.target.value)}
+          value={username}
+        />
       </FormField>
+
       <FormField label="Email">
-        <input />
+        <input
+          type="email"
+          name="email"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          value={email}
+        />
       </FormField>
+
       <FormField label="Пароль">
-        <input type="password" />
+        <input
+          type="password"
+          name="password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          value={password}
+        />
       </FormField>
-      <Button>Зарегистрироваться</Button>
+      <Button type="submit">Зарегистрироваться</Button>
     </form>
   );
 };
