ceb9df11428b1eebbfd66db9ac33b47bdee7e911
Igor Storchevoy <hellsgor@gmail.com>
2024-07-23 00:26:36 +0400

    Add: create Account component

---
+++ client/src/components/Account/Account.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchMe } from '../../api/User';
+import { Loader } from '../Loader';
+import { AuthForm } from '../AuthForm';
+import { UserView } from '../UserView';
+import { queryClient } from '../../api/queryClient';
+import { LogoutButton } from '../LogoutButton';
+
+export function Account() {
+  const meQuery = useQuery(
+    {
+      queryFn: () => fetchMe(),
+      queryKey: ['users', 'me'],
+    },
+    queryClient,
+  );
+
+  switch (meQuery.status) {
+    case 'pending':
+      return <Loader />;
+
+    case 'error':
+      return <AuthForm />;
+
+    case 'success':
+      return (
+        <>
+          <UserView />
+          <LogoutButton />
+        </>
+      );
+  }
+}
