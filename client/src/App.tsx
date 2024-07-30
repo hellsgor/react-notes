c50f81b0635e88e71a56be9d71c960333480db5a
Igor Storchevoy <hellsgor@gmail.com>
2024-07-30 14:26:36 +0400

    FIx: component double lead

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css';
 import { Account } from './components/Account/Account';
-import { AuthForm } from './components/AuthForm';
 
 function App() {
   return (
     <div className="app">
       <Account />
-
-      <AuthForm />
     </div>
   );
 }
