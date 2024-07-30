ce4cf4f4623b2b1ff67cbe9fee1d5aa198892857
Igor Storchevoy <hellsgor@gmail.com>
2024-07-30 22:57:53 +0400

    Update: typified useAuthType hook

--- client/src/hooks/useAuthType.ts
+++ client/src/hooks/useAuthType.ts
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export type AuthType = 'register' | 'auth';
 type UseAuthTypeProps = AuthType | null;
 
-export function useAuthType(initialValue: AuthType) {
+export function useAuthType(initialValue: UseAuthTypeProps = null) {
   const [authType, setAuthType] = useState(initialValue);
 
   const switchAuthType = (newAuthType: UseAuthTypeProps = null) => {
