163827857e77848d01b56476d1cbf9ef2f620151
Igor Storchevoy <hellsgor@gmail.com>
2024-07-30 17:36:06 +0400

    Update: formatted AuthForm

--- client/src/components/AuthForm/AuthForm.tsx
+++ client/src/components/AuthForm/AuthForm.tsx
@@ -1,30 +1,30 @@
-import { useState } from "react";
-import { LoginForm } from "../LoginForm";
-import { RegisterForm } from "../RegisterForm";
+import { useState } from 'react';
+import { LoginForm } from '../LoginForm';
+import { RegisterForm } from '../RegisterForm';
 
-import "./AuthForm.css";
+import './AuthForm.css';
 
 export const AuthForm = () => {
-  const [authType, setAuthType] = useState<string>("register");
+  const [authType, setAuthType] = useState<string>('register');
 
   const handleClick = () => {
     setAuthType((prevState) =>
-      prevState === "register" ? "auth" : "register",
+      prevState === 'register' ? 'auth' : 'register',
     );
   };
 
   return (
     <div className="auth-form">
       <p className="auth-form__title">
-        {authType === "register" ? "Регистрация" : "Авторизация"}
+        {authType === 'register' ? 'Регистрация' : 'Авторизация'}
       </p>
-      {authType === "register" ? <RegisterForm /> : <LoginForm />}
+      {authType === 'register' ? <RegisterForm /> : <LoginForm />}
       <div className="auth-form__info">
         <span>
-          {authType === "register" ? "Уже есть аккаунт?" : "Ещё нет аккаунта?"}
+          {authType === 'register' ? 'Уже есть аккаунт?' : 'Ещё нет аккаунта?'}
         </span>
         <button className="auth-form__button" onClick={handleClick}>
-          {authType === "register" ? "Войти" : "Создать аккаунт"}
+          {authType === 'register' ? 'Войти' : 'Создать аккаунт'}
         </button>
       </div>
     </div>
