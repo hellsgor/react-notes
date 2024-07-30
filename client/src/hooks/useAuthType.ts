28789d8706eb9262f0bbb93b0662984d3505b33e
Igor Storchevoy <hellsgor@gmail.com>
2024-07-30 17:40:19 +0400

    Add: create useAuthType hook

---
+++ client/src/hooks/useAuthType.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export type AuthType = 'register' | 'auth';
+type UseAuthTypeProps = AuthType | null;
+
+export function useAuthType(initialValue: AuthType) {
+  const [authType, setAuthType] = useState(initialValue);
+
+  const switchAuthType = (newAuthType: UseAuthTypeProps = null) => {
+    setAuthType(
+      newAuthType ? newAuthType : authType === 'register' ? 'auth' : 'register',
+    );
+  };
+
+  return { authType, setAuthType, switchAuthType } as const;
+}
