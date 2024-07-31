f55f74d1d953e03f3d677bcd8ed858f7b64e84c4
Igor Storchevoy <hellsgor@gmail.com>
2024-07-31 11:01:26 +0400

    Update: using useMeQuery in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import { Account } from './components/Account/Account';
+import { useMeQuery } from './hooks/useMeQuery';
 
 function App() {
+  const meQuery = useMeQuery();
+
   return (
     <div className="app">
-      <Account />
+      <Account meQuery={meQuery} />
     </div>
   );
 }
--- client/src/components/Account/Account.tsx
+++ client/src/components/Account/Account.tsx
@@ -1,21 +1,16 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchMe } from '../../api/User';
+import { UseQueryResult } from '@tanstack/react-query';
+import { User } from '../../api/User';
 import { Loader } from '../Loader';
 import { AuthForm } from '../AuthForm';
 import { UserView } from '../UserView';
-import { queryClient } from '../../api/queryClient';
 import { LogoutButton } from '../LogoutButton';
+import { FC } from 'react';
 
-export function Account() {
-  const meQuery = useQuery(
-    {
-      queryFn: () => fetchMe(),
-      queryKey: ['users', 'me'],
-      retry: 0,
-    },
-    queryClient,
-  );
+interface AccountProps {
+  meQuery: UseQueryResult<User>;
+}
 
+export const Account: FC<AccountProps> = ({ meQuery }) => {
   switch (meQuery.status) {
     case 'pending':
       return (
@@ -43,4 +38,4 @@ export function Account() {
         </>
       );
   }
-}
+};
