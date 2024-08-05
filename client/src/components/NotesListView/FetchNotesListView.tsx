6a9c5dbb772d03b8f577973fd9a6ed1469732068
Igor Storchevoy <hellsgor@gmail.com>
2024-08-05 23:14:11 +0400

    Add: added useNotesQuery hook

--- client/src/components/NotesListView/FetchNotesListView.tsx
+++ client/src/components/NotesListView/FetchNotesListView.tsx
@@ -1,18 +1,12 @@
-import { useQuery } from '@tanstack/react-query';
-import { getNotes } from '../../api/Note';
-import { queryClient } from '../../api/queryClient';
 import { Loader } from '../Loader';
 import { NotesListView } from './NotesListView';
 import { FC } from 'react';
+import { useNotesQuery } from '../../hooks/useNotesQuery';
+import { UseQueryResult } from '@tanstack/react-query';
+import { NotesResponse } from '../../api/Note';
 
 export const FetchNotesListView: FC = () => {
-  const notesQuery = useQuery(
-    {
-      queryFn: () => getNotes(),
-      queryKey: ['notes'],
-    },
-    queryClient,
-  );
+  const notesQuery: UseQueryResult<NotesResponse> = useNotesQuery();
 
   switch (notesQuery.status) {
     case 'pending':
@@ -30,7 +24,11 @@ export const FetchNotesListView: FC = () => {
       );
 
     case 'error':
-      return <span>Что-то пошло не так</span>;
+      return (
+        <span style={{ color: 'var(--error)' }}>
+          {notesQuery.error.message}
+        </span>
+      );
 
     case 'success':
       return <NotesListView notesList={notesQuery.data.list} />;
